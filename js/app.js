809334e0eb279e6152ae29f3e825468c3d0e50f2
Clara Munro <clarajmunro@gmail.com>
2018-12-04 15:44:52 -0800

    First 2 stores done

--- js/app.js
+++ js/app.js
@@ -1,27 +1,72 @@
 'use strict';
 
-var months = ['May','June','July','August','September'];
+var openHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
-var twoThousandEighteen = {
-  miles: [54, 12, 9000, 8, 52],
+var firstAndPike =  {
+  minCust: 23,
+  maxCust: 65,
+  avgCookiePerCust: 6.3,
 
-  render: function() {
-    var ulElement = document.getElementById('list2018');
+  randomCustPerHour: function() {
+    return Math.round( Math.random() * (this.maxCust - this.minCust) + this.minCust );
+  },
 
-    // Goal: create list items and add them to the DOM
-    for(var i = 0; i < this.miles.length; i++){
+  averagePerHour: function() {
+    return Math.round (this.randomCustPerHour() * this.avgCookiePerCust );
+  },
 
-      // 1. Access / create the element
+  generateList: function() {
+    var ulElement = document.getElementById('first-pike');
+    var runningTotal = 0;
+
+    for(var i = 0; i < openHours.length; i++){
       var liElement = document.createElement('li');
+      var randomNum = this.averagePerHour();
+      runningTotal += randomNum;
+      liElement.textContent = `${openHours[i]}: ${randomNum} cookies!`;
 
-      // 2. Give the element content
-      liElement.textContent = `In ${months[i]}, I hiked ${this.miles[i]} miles.`;
-
-      // 3. Append the element to its parents (usually to its parent)
       ulElement.appendChild(liElement);
     }
 
+    var totalEl = document.createElement('li');
+    totalEl.textContent = `Total for the day: ${runningTotal}`;
+    ulElement.appendChild(totalEl);
   }
+
 };
 
-twoThousandEighteen.render();
+var SeaTac =  {
+  minCust: 3,
+  maxCust: 24,
+  avgCookiePerCust: 1.2,
+
+  randomCustPerHour: function() {
+    return Math.round( Math.random() * (this.maxCust - this.minCust) + this.minCust );
+  },
+
+  averagePerHour: function() {
+    return Math.round (this.randomCustPerHour() * this.avgCookiePerCust );
+  },
+
+  generateList: function() {
+    var ulElement = document.getElementById('Sea-Tac');
+    var runningTotal = 0;
+
+    for(var i = 0; i < openHours.length; i++){
+      var liElement = document.createElement('li');
+      var randomNum = this.averagePerHour();
+      runningTotal += randomNum;
+      liElement.textContent = `${openHours[i]}: ${randomNum} cookies!`;
+
+      ulElement.appendChild(liElement);
+    }
+
+    var totalEl = document.createElement('li');
+    totalEl.textContent = `Total for the day: ${runningTotal}`;
+    ulElement.appendChild(totalEl);
+  }
+
+};
+
+firstAndPike.generateList();
+SeaTac.generateList();
