828dd2142bd60cc5ddee60801dc3ceebd5f88873
Clara Munro <clarajmunro@gmail.com>
2018-12-04 16:13:04 -0800

    Added Store 3

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 
 var openHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
+
+// First & Pike Store 1
+
 var firstAndPike =  {
   minCust: 23,
   maxCust: 65,
@@ -35,7 +38,11 @@ var firstAndPike =  {
 
 };
 
-var SeaTac =  {
+
+// SeaTac Store 2
+
+var seaTac =  {
+  storeName: 'SeaTac',
   minCust: 3,
   maxCust: 24,
   avgCookiePerCust: 1.2,
@@ -49,7 +56,42 @@ var SeaTac =  {
   },
 
   generateList: function() {
-    var ulElement = document.getElementById('Sea-Tac');
+    var ulElement = document.getElementById('sea-tac');
+    var runningTotal = 0;
+
+    for(var i = 0; i < openHours.length; i++){
+      var liElement = document.createElement('li');
+      var randomNum = this.averagePerHour();
+      runningTotal += randomNum;
+      liElement.textContent = `${openHours[i]}: ${randomNum} cookies!`;
+
+      ulElement.appendChild(liElement);
+    }
+
+    var totalEl = document.createElement('li');
+    totalEl.textContent = `Total for the day: ${runningTotal}`;
+    ulElement.appendChild(totalEl);
+  }
+
+};
+
+// Seattle Center Store 3
+
+var seattleCenter =  {
+  minCust: 3,
+  maxCust: 24,
+  avgCookiePerCust: 1.2,
+
+  randomCustPerHour: function() {
+    return Math.round( Math.random() * (this.maxCust - this.minCust) + this.minCust );
+  },
+
+  averagePerHour: function() {
+    return Math.round (this.randomCustPerHour() * this.avgCookiePerCust );
+  },
+
+  generateList: function() {
+    var ulElement = document.getElementById('seattle-center');
     var runningTotal = 0;
 
     for(var i = 0; i < openHours.length; i++){
@@ -69,4 +111,5 @@ var SeaTac =  {
 };
 
 firstAndPike.generateList();
-SeaTac.generateList();
+seaTac.generateList();
+seattleCenter.generateList();
