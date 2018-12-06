d4af4c1d92ede67d2143a601b8a6212a77c7d9e3
Clara Munro <clarajmunro@gmail.com>
2018-12-05 21:50:39 -0800

    Finished the table, footer row & totals

--- js/app.js
+++ js/app.js
@@ -1,113 +1,118 @@
-'use strict';
+'use-strict';
 
 // Global Variables
-var storeTable = document.getElementById('table');
 
-// Constructor Functions
-function Cookies(name, minCust, maxCust, avgCookiePerCust) {
+// Link table with id #table from HTML to a JS variable
+var salesTable = document.getElementById('table');
+
+// Store hours, X-axis
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// adds the footer #'s and store total #'s together
+var grandArray = [];
+
+
+// Constructor Function
+function BuildStore (name, minCust, maxCust, avgCookiePerSale) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
-  this.avgCookiePerCust = avgCookiePerCust;
-  Cookies.openHours.push(this);
+  this.avgCookiePerSale = avgCookiePerSale;
+  this.totalCookiesPerStore = 0;
+  this.cookiesPerHour = [];
+  BuildStore.allStores.push(this);
 }
 
-Cookies.openHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+BuildStore.allStores = [];
 
-// Methods
-// Find the random customer per hour
-Cookies.prototype.randomCustPerHour = function() {
-  return Math.round( Math.random() * (this.maxCust - this.minCust) + this.minCust );
-},
+// render a ROW & add CELLS that are random number generated based on the array
+BuildStore.prototype.render = function() {
+  var trEl = document.createElement('tr'); // make a tr (row)
+  var tdEl = document.createElement('td'); // make a td (cell)
+  tdEl.textContent = this.name; // fill the td (cell) with the name
+  trEl.appendChild(tdEl); // link the td (cell) to the tr (row)
+  salesTable.appendChild(trEl); // link the tr (row) to the form (salesTable)
 
-// Find the average customer per hour
-Cookies.prototype.averagePerHour = function() {
-  return Math.round (this.randomCustPerHour() * this.avgCookiePerCust );
-},
+  for(var i = 0; i < hours.length; i++) { // for each hour index (x-axis), generate a random #)
+    tdEl = document.createElement('td'); // make a td (cell)
+    var randomNumber = Math.floor( (Math.floor( Math.random() * (this.maxCust - this.minCust) + this.minCust) ) * this.avgCookiePerSale );
+    tdEl.textContent = randomNumber; // add this random # for the content to the td
+    trEl.appendChild(tdEl); // add this td to the tr
+    this.cookiesPerHour.push(randomNumber); // put each random # in the cookiesPerHour array
+    this.totalCookiesPerStore += randomNumber; // add these #s up to equal the total cookies per day per store
+  }
+  //  this section ADDS THE #'s together and displays it's content in the CELL (AT THE END)
+  tdEl = document.createElement('td'); // create a cell
+  tdEl.textContent = this.totalCookiesPerStore; // add # total cookies per store to the td
+  trEl.appendChild(tdEl); // link this td to the tr
+  grandArray.push(this.totalCookiesPerStore); // put this # (each total cookies per store) into the grand total array
+};
 
-// Generate the list & append to table
-Cookies.prototype.generateList = function() {
-  // create a tr
-  var trEl = document.createElement('tr');
-  // create a td
-  var tdEl = document.createElement('td');
 
-  // append various elements to td
-  for (var i = 0; i < Cookies.openHours.length; i++) {
-  // do this one time = average # of stuff
-    for (var j = 0; j < Cookies.openHours.length ; i++){
-      // something
-      
+// render header ROW to salesTable
+BuildStore.renderHeader = function() {
+  var headerRow = document.createElement('tr'); // create a row
+
+  for (var i = -1; i < hours.length; i++) { // for each index in the hours array
+    var thEl = document.createElement('th'); // create a cell
+    thEl.textContent = hours[i]; // assign the cell the array text content (which is the time)
+    headerRow.appendChild(thEl); // add these cells to the headerRow
+  }
+  // make the end cell block with label
+  thEl = document.createElement('th'); // make a cell
+  thEl.textContent = 'Store Totals:'; // put 'Store Totals' in it
+  headerRow.appendChild(thEl); // add it to the row of th's
+  salesTable.prepend(headerRow); // add the whole row to the table
+};
+
+
+// RENDER ALL!
+BuildStore.renderAll = function() {
+  for (var i = 0; i < BuildStore.allStores.length; i++) {
+    BuildStore.allStores[i].render();
+  }
+};
+
+
+// Render the footer ROW (adds totals for EACH HOUR)
+BuildStore.renderFooter = function() {
+  // make the first cell with the label
+  var footerRow = document.createElement('tr'); // make a row
+  var tdEl = document.createElement('td'); // make a cell
+  tdEl.textContent = 'Total Per Hour:'; // add text to the first cell
+  footerRow.appendChild(tdEl); // all this cell to the row
+  salesTable.appendChild(footerRow); // add this row to the table
+
+  // iterate through the array, adding each total per hour, add to cell
+  for (var x = 0; x < hours.length; x++) { // for each hour
+    var counter = 0;
+    for (var j = 0; j < BuildStore.allStores.length; j++) {  // for each store
+      counter += BuildStore.allStores[j].cookiesPerHour[x];  // j = each store index, x = each cookiePerHour index
     }
-    storeTable.appendChild(trEl);
+
+    tdEl = document.createElement('td');  // create a cell
+    tdEl.textContent = counter; // put the counter # as text
+    footerRow.appendChild(tdEl);  // add cell to the row
   }
+
+  // adds the end cell which is the GRAND TOTAL of the END ROW (footer) & END CELL (per store)
+  var grandGrand = 0;
+  for (var i = 0; i < grandArray.length; i++) {  // for each # in the array
+    grandGrand += grandArray[i];  // add everything in grandArray up to equal grandGrand
+  }
+  tdEl = document.createElement('td');  // make a cell
+  tdEl.textContent = grandGrand;  // put the total variable # in
+  footerRow.appendChild(tdEl); // add it to the row
 };
 
 
+new BuildStore('1st & Pike', 23, 65, 6.3);
+new BuildStore('SeaTac Airport', 3, 24, 1.2);
+new BuildStore('Seattle Center', 11, 38, 3.7);
+new BuildStore('Capitol Hill', 20, 38, 2.2);
+new BuildStore('Alki', 2, 16, 4.6);
 
 
-Cookies.renderHeader = function() {
-  var headerRow = document.createElement('tr');
-
-  var headings = ['1st & Pike','SeaTac Airport','Seattle Center', 'Capitol Hill', 'Alki'];
-
-  for( var i = 0; i < Cookies.openHours.length; i++) {
-    var thEl = document.createElement('th');
-    thEl.textContent = headings[1];
-    headerRow.appendChild(thEl);
-
-  }
-  // append row to the table
-  storeTable.appendChild(headerRow);
-
-};
-
-Cookies.renderFooter = function() {
-
-  var ulElement = document.getElementById('first-pike');
-  var runningTotal = 0;
-
-  for(var i = 0; i < Cookies.openHours.length; i++){
-    var liElement = document.createElement('li');
-    var randomNum = this.averagePerHour();
-    runningTotal += randomNum;
-    liElement.textContent = `${Cookies.openHours[i]}: ${randomNum} cookies!`;
-
-    ulElement.appendChild(liElement);
-  }
-  var totalEl = document.createElement('li');
-  totalEl.textContent = `Daily Location Total: ${runningTotal}`;
-  ulElement.appendChild(totalEl);
-  
-}
-
-
-
-
-
-// instances
-new Cookies('1st & Pike', 23, 65, 6.3);
-new Cookies('SeaTac Airport', 3, 24, 1.2);
-new Cookies('Seattle Center', 11, 38, 3.7);
-new Cookies('Capitol Hill', 20, 38, 2.3);
-new Cookies('Alki', 2, 16, 4.6);
-
-
-// invoke methods
-Cookies.renderHeader();
-
-// firstAndPike.generateList();
-// seaTac.generateList();
-// seattleCenter.generateList();
-// capitolHill.generateList();
-// alki.generateList();
-
-Cookies.renderAll = function () {
-  for(var i = 0; i < openHours.length; i++){
-    Cookies.openHours[i].render();
-  }
-};
-
-Cookies.renderAll();
-
-// Cookies.renderFooter();
+BuildStore.renderHeader();
+BuildStore.renderAll();
+BuildStore.renderFooter();
